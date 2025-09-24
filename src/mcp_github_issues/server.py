7ffa382cd75ba567df027cbd2fdf39f13c8a40b8
Yuriy Serdyuk <yury.m.serdyuk@gmail.com>
2025-09-24 22:08:09 +0100

    #2 - Updated Server with main()

--- src/mcp_github_issues/server.py
+++ src/mcp_github_issues/server.py
@@ -1,11 +1,14 @@
-# src/server.py
+# src/mcp_github_issues/server.py
 
 from mcp.server import FastMCP
 from .tools.get_issue import get_issue
 
 mcp = FastMCP("mcp-github-issues")
-
 mcp.add_tool(get_issue)
 
+def main():
+    """Entry point for uvx (console script)."""
+    mcp.run(transport="stdio")
+
 if __name__ == "__main__":
-    mcp.run(transport='stdio')
+    main()
